bd2c08f6536226c2bc89de0932f55d1c39dd0885
Lam Minh Man <lamminhmanie@gmail.com>
2025-09-06 23:56:36 +0700

    lesson 25 React - Youtube code

--- freeCodeCamp/react-project-beginner/src/component/Main.jsx
+++ freeCodeCamp/react-project-beginner/src/component/Main.jsx
@@ -1,5 +1,14 @@
 export default function Main() {
     return(
-        <h1>Main goes here</h1>
+        <main>
+            <h1>Fun facts about React</h1>
+            <ul className="facts-list">
+                <li>Was first released in 2013</li>
+                <li>Was original created by Jordan Walke</li>
+                <li>Has well over 200k stars on Github</li>
+                <li>Is maintain by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
     )
 }
